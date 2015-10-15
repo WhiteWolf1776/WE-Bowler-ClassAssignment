cb3ff6739c4147021d221aebe58cc9cbe57f61e2
Jeremy Foster <jeremy.foster@fotlinc.com>
2015-10-15 13:04:17 -0500

    Updated validate_input files to check for simple return 0

--- js/validate_input.js
+++ js/validate_input.js
@@ -1,3 +1,7 @@
 /**
  * Created by nbliz0 on 10/15/2015.
  */
+
+function return0 () {
+  return 0;
+}
--- test/validate_input_test.js
+++ test/validate_input_test.js
@@ -1,3 +1,14 @@
 /**
  * Created by nbliz0 on 10/15/2015.
  */
+
+var assert = chai.assert;
+
+describe( 'Validating Input', function()  {
+
+  it( 'should validate user input', function() {
+    var result = return0();
+    assert.equal( result, 0 );
+  } );
+  
+} );
