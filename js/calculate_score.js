66ca18ec2b8eb9ff5827400991b15d1cd5766c6f
Nick Blizzard <nick.blizzard@fotlinc.com>
2015-10-15 12:11:58 -0500

    initial tests added... time to CODE

---
+++ js/calculate_score.js
@@ -0,0 +1,9 @@
+/**
+ * Created by nbliz0 on 10/15/2015.
+ */
+
+function return0() {
+    return 0;
+}
+
+
---
+++ test/bowlingScore.js
@@ -0,0 +1,14 @@
+/**
+ * Created by nbliz0 on 10/15/2015.
+ */
+
+var assert = chai.assert;
+
+describe( 'Scoring Tests', function() {
+
+    it( 'should calculate individual frame scores', function() {
+        var result = return0();
+        assert.equal( result, 0);
+    } );
+
+} );
