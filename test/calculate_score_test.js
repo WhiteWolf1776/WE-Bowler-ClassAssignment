ac083d866b542e68d2771935c04cc31891ea21b8
Nick Blizzard <nick.blizzard@fotlinc.com>
2015-10-15 14:57:59 -0500

    changed data to ints instead of strings

--- test/calculate_score_test.js
+++ test/calculate_score_test.js
@@ -5,45 +5,43 @@
 var assert = chai.assert;
 var rolls;
 
-var perfectGame = [[10],[10],[10],[10],[10],
-                   [10],[10],[10],[10],[10],[10],[10]];
+var perfectGame = [[10,0],[10,0],[10,0],[10,0],[10,0],
+                   [10,0],[10,0],[10,0],[10,0],[10,10,10]];
 
-var twentyFrameGame = [['1','1'],['1','1'],['1','1'],['1','1'],['1','1'],
-                       ['1','1'],['1','1'],['1','1'],['1','1'],['1','1']];
+var twentyFrameGame = [[1,1],[1,1],[1,1],[1,1],[1,1],
+                       [1,1],[1,1],[1,1],[1,1],[1,1]];
 
-var gameWithOneBonusPin = [['9','1'],['1','0'],['0','0'],['0','0'],['0','0'],
-                           ['0','0'],['0','0'],['0','0'],['0','0'],['0','0']];
+var gameWithOneBonusPin = [[9,1],[1,0],[0,0],[0,0],[0,0],
+                           [0,0],[0,0],[0,0],[0,0],[0,0]];
 
-var gameWithTwoBonusPins = [['10'],['1','1'],['0','0'],['0','0'],['0','0'],
-                            ['0','0'],['0','0'],['0','0'],['0','0'],['0','0']];
+var gameWithTwoBonusPins = [[10,0],[1,1],[0,0],[0,0],[0,0],
+                            [0,0],[0,0],[0,0],[0,0],[0,0]];
 
 describe( 'Scoring Tests', function() {
 
     it( 'should calculate score for first frame', function() {
-        rolls = [['3', '5']];
-        rolls = calculateScore(rolls);
-        assert.equal( rolls[0][3], '8');
+        rolls = calculateScore(twentyFrameGame);
+        assert.equal( rolls[0][3], 2);
     } );
 
     it( 'should calculate score for two frames that do not include strike or spare', function() {
-        rolls = [['3', '5'],
-                 ['2','6']];
-        rolls = calculateScore(rolls);
-        assert.equal( rolls[1][3], '16');
+        rolls = calculateScore(twentyFrameGame);
+        assert.equal( rolls[1][3], 4);
     });
 
     it( 'should calculate the score for all 10 frames including no spares or strikes', function() {
-        rolls = [['5', '3'],
-                 ['5', '3'],
-                 ['5', '3'],
-                 ['5', '3'],
-                 ['5', '3'],
-                 ['5', '3'],
-                 ['5', '3'],
-                 ['5', '3'],
-                 ['5', '3'],
-                 ['5', '3']];
-        rolls = calculateScore(rolls);
-        assert.equal( rolls[9][3], '80');
-    })
+        rolls = calculateScore(twentyFrameGame);
+        assert.equal( rolls[9][3], 20);
+    });
+
+    it ('it should calculate a game with a spare and one bonus pin', function(){
+        rolls = calculateScore(gameWithOneBonusPin);
+        assert.equal( rolls[9][3], 12);
+    });
+
+    it ('it should calculate a game with a strike and two bonus pins', function(){
+        rolls = calculateScore(gameWithTwoBonusPins);
+        assert.equal( rolls[9][3], 14);
+    });
+
 } );
